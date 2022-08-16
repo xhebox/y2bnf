86a4b7cd2a3700f2c259dbec9e1146e49d81a2e4
xhe <xw897002528@gmail.com>
2022-08-16 17:41:24 +0800

    init
    Signed-off-by: xhe <xw897002528@gmail.com>

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/xhebox/y2bnf
+
+go 1.18
+
+require modernc.org/y v1.0.3
+
+require (
+	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	modernc.org/golex v1.0.1 // indirect
+	modernc.org/mathutil v1.4.1 // indirect
+	modernc.org/parser v1.0.2 // indirect
+	modernc.org/sortutil v1.1.0 // indirect
+	modernc.org/strutil v1.1.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"go/token"
+	"log"
+	"sort"
+	"strings"
+
+	"modernc.org/y"
+)
+
+var termName = map[string]string{
+	"identifier":         "identifier",
+	"singleAtIdentifier": "singleAtIdentifier",
+	"doubleAtIdentifier": "doubleAtIdentifier",
+	"invalid":            "invalid",
+	"hintComment":        "hintComment",
+	"stringLit":          "stringLit",
+	"floatLit":           "floatLit",
+	"decLit":             "decLit",
+	"intLit":             "intLit",
+	"hexLit":             "hexLit",
+	"bitLit":             "bitLit",
+}
+
+func main() {
+	yaccFile := flag.String("in", "parser.y", "yacc file")
+	recursive := flag.Bool("recursive", true, "automatically add sub items")
+	_include := flag.String("include", "", "list of symbols needs to be output, separated by comma")
+	_exclude := flag.String("exclude", "Identifier,Expression,BitExpr", "list of symbols that should not be output, separated by comma")
+	flag.Parse()
+	include := strings.Split(*_include, ",")
+	exclude := strings.Split(*_exclude, ",")
+	sort.Strings(exclude)
+
+	p, err := y.ProcessFile(token.NewFileSet(), *yaccFile, &y.Options{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := &bytes.Buffer{}
+	dup := make(map[string]struct{}, len(include))
+	for i := 0; i < len(include); i++ {
+		v := p.Syms[include[i]]
+		if v == nil || v.IsTerminal {
+			continue
+		}
+		if k := sort.SearchStrings(exclude, v.Name); k != len(exclude) && exclude[k] == v.Name {
+			continue
+		}
+		dup[v.Name] = struct{}{}
+
+		lines.Reset()
+
+		fmt.Fprintf(lines, "%s ::=\n", v.Name)
+		for i, rule := range v.Rules {
+			fmt.Fprintf(lines, "\t ")
+			if i > 0 {
+				fmt.Fprintf(lines, "| ")
+			}
+			if len(rule.Components) == 0 {
+				fmt.Fprintf(lines, "/* empty */")
+			}
+			for _, c := range rule.Components {
+				sym := p.Syms[c]
+				if sym.IsTerminal {
+					v := termName[sym.Name]
+					if v != "" {
+						fmt.Fprintf(lines, "%s ", v)
+					} else if sym.LiteralString != "" {
+						fmt.Fprintf(lines, "%s ", sym.LiteralString)
+					} else {
+						fmt.Fprintf(lines, "%s ", sym.Name)
+					}
+				} else {
+					fmt.Fprintf(lines, "%s ", sym.Name)
+				}
+				if *recursive {
+					if _, ok := dup[sym.Name]; !ok {
+						include = append(include, sym.Name)
+						dup[sym.Name] = struct{}{}
+					}
+				}
+			}
+			fmt.Fprintf(lines, "\n")
+		}
+
+		fmt.Println(lines.String())
+	}
+}
